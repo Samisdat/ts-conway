07577fab9ea7416ee7ad790dc06892f28794d72e
samisdat <bastian.sackermann@gmail.com>
2016-10-24 22:03:14 +0000

    complete test with test to cover elapsing time and kill

--- tests/cell.ts
+++ tests/cell.ts
@@ -22,6 +22,51 @@ describe('Cell', () => {
 
     });
 
+    it('get x and y', () => {
 
+        let position = new Position(1, 2);
+        let cell =  new Cell(position);
+        
+        expect(cell.x).to.be.equal(position.x);
+        expect(cell.y).to.be.equal(position.y);
+        expect(cell.position).to.be.equal(position);
+
+    });
+
+    describe('livetime', () => {
+
+        let position = new Position(1, 2);
+
+        let cell;
+
+        before(() => {
+
+            cell =  new Cell(position);
+
+        });
+
+        it('alive some time after  creation', () => {
+    
+            expect(cell.isAlive()).to.be.false;
+            
+            cell.elapse();
+            expect(cell.isAlive()).to.be.true;
+
+        });
+
+        it('kill', () => {
+
+            cell.elapse();
+            expect(cell.isAlive()).to.be.true;
+    
+            cell.kill();
+            expect(cell.isAlive()).to.be.true;
+
+            cell.elapse();
+            expect(cell.isAlive()).to.be.false;
+
+        });
+
+    });
 
 });
