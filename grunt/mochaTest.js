ee185d48817165b4bb2827ee2932a7b533279e97
Bastian Pertz <bastian.pertz@vodafone.com>
2016-10-24 20:59:09 +0200

    add a test from http://jonnyreeves.co.uk/2015/hello-typescript-and-mocha/ add grunt task for mocha test add tsd type mappings add mocha dependecies

---
+++ grunt/mochaTest.js
@@ -0,0 +1,28 @@
+var path = require('path');
+
+var mocha = {
+    options: {
+        reporter: 'spec',
+        captureFile: 'results.txt', // Optionally capture the reporter output to a file
+        quiet: false, // Optionally suppress output to standard out (defaults to false)
+        clearRequireCache: true // Optionally clear the require cache before running tests (defaults to false)
+    },
+    'typescript': {
+        src: [
+            'test/*.js'
+        ]
+    }
+};
+
+module.exports = function(grunt){
+
+    'use strict';
+    /*
+    grunt.event.on('watch', function(action, filepath){
+
+
+    });
+    */
+    return mocha;
+
+};
