3b396b9340c71f873eb6917633af03ef0d161081
Bastian Pertz <bastian.pertz@vodafone.com>
2016-10-24 20:34:19 +0200

    DEVELOP: add .ts from http://jonnyreeves.co.uk/2015/hello-typescript-and-mocha/

---
+++ ts/index.ts
@@ -0,0 +1,8 @@
+export default class Calculator {
+    add(x : number, y : number) : number {
+        return x + y;
+    }
+    sub(x : number, y : number) : number {
+        return x - y;
+    }
+}
