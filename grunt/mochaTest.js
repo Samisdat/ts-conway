806458eae7d7ca049716634c30e86afd0ec72e8a
Bastian Pertz <bastian.pertz@vodafone.com>
2016-10-24 22:03:10 +0200

    don't dump stdout of mocha test to results.txt

--- grunt/mochaTest.js
+++ grunt/mochaTest.js
@@ -3,7 +3,6 @@ var path = require('path');
 var mocha = {
     options: {
         reporter: 'spec',
-        captureFile: 'results.txt', // Optionally capture the reporter output to a file
         quiet: false, // Optionally suppress output to standard out (defaults to false)
         clearRequireCache: true // Optionally clear the require cache before running tests (defaults to false)
     },
