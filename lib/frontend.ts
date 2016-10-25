be03069d987e452e7b436c248556d654392e0368
Bastian Pertz <bastian.pertz@vodafone.com>
2016-10-25 23:35:31 +0200

    a view into a habitat

---
+++ lib/frontend.ts
@@ -0,0 +1,111 @@
+import Position from './position';
+import Habitat from './habitat';
+
+export default class Frontend {
+
+    private habitat: Habitat;
+
+    private cols: number;
+    private rows: number;
+
+    private zero: Position;
+
+    private pan: Position;
+    
+    constructor(cols:number, rows:number, habitat: Habitat) {
+        this.cols = cols
+        this.rows = rows
+
+        this.zero = new Position(
+            Math.floor(this.cols/2),
+            Math.floor(this.rows/2)
+        );
+        
+        this.pan = new Position(
+            0,
+            0
+        );
+
+        this.habitat = habitat
+    }
+
+    public elapse():void {
+
+        this.habitat.elapse();
+
+    }
+
+    public seed(position:Position):void {
+        this.habitat.seed(position);
+    }
+
+    public map(position:Position):Position{
+
+        let mapped = this.zero.move(position);
+        let moved = mapped.move(this.pan);
+
+        return moved;
+    }
+
+    public get():Position[] {
+
+        var mapped:Position[] = [];     
+
+        var living:Position[] = this.habitat.get();
+
+        for(let position of living){
+            let map = this.map(position);
+
+            mapped.push(map);
+            
+        }
+
+        return mapped;
+    }
+
+    toString():string{
+        let matrix = [];
+
+        for(let y = 0; y < this.rows; y += 1){
+
+            let row = [];
+
+            for(let x = 0; x < this.cols; x += 1){
+                row.push('0');
+            }
+
+            matrix.push(row);
+
+        }
+
+        var living:Position[] = this.get();
+
+        for(let position of living){
+            matrix[position.y][position.x] = 'X';
+        }
+
+        let out = [];
+
+        out.push("\n");
+
+        for(let row of matrix){
+
+            let outRow = [];
+            for(let cell of row){
+                outRow.push("+-");
+            }
+            outRow.push("+\n");
+            
+            for(let cell of row){
+                outRow.push('|');
+                outRow.push(cell);
+            }
+            outRow.push('|');
+            
+            out.push(outRow.join(''));
+        }
+
+        return out.join("\n")   
+    }
+
+}
---
+++ tests/frontend.ts
@@ -0,0 +1,142 @@
+/// <reference path="../typings/mocha/mocha.d.ts" />
+/// <reference path="../typings/chai/chai.d.ts" />
+
+import { expect } from 'chai';
+
+import Position from '../lib/position';
+import Frontend from '../lib/frontend';
+import Habitat from '../lib/habitat';
+
+describe('Frontend', () => {
+
+    it('can be created', () => {
+
+        let frontend = new Frontend(
+            25, 
+            10, 
+            new Habitat()
+        );
+
+    });
+
+    it('seeding top right corner', () => {
+
+        let frontend = new Frontend(
+            3, 
+            3, 
+            new Habitat()
+        );
+
+        expect(frontend.get()).to.be.deep.equal([]);
+        
+        /** 
+         * seeding
+         * X00
+         * 000
+         * 000
+         */
+        frontend.seed(
+            new Position(-1, -1)
+        );
+
+        expect(frontend.get()).to.be.deep.equal([
+            new Position(0, 0),
+        ]);
+
+        console.log(frontend.toString());
+
+
+    });
+
+    it('seeding all corners', () => {
+
+        let frontend = new Frontend(
+            3, 
+            3, 
+            new Habitat()
+        );
+
+        expect(frontend.get()).to.be.deep.equal([]);
+        
+        /** 
+         * seeding
+         * X0X
+         * 000
+         * X0X
+         */
+        frontend.seed(
+            new Position(-1, -1)
+        );
+
+        frontend.seed(
+            new Position(1, -1)
+        );
+
+        frontend.seed(
+            new Position(-1, 1)
+        );
+
+        frontend.seed(
+            new Position(1, 1)
+        );
+
+        expect(frontend.get()).to.be.deep.equal([
+            new Position(0, 0),
+            new Position(2, 0),
+            new Position(0, 2),
+            new Position(2, 2),
+        ]);
+
+        console.log(frontend.toString());
+
+
+    });
+
+    it('elapse with a blinker', () => {
+
+        let frontend = new Frontend(
+            3, 
+            3, 
+            new Habitat()
+        );
+
+        expect(frontend.get()).to.be.deep.equal([]);
+        
+        frontend.seed(
+            new Position(0, -1)
+        );
+        frontend.seed(
+            new Position(0, 0)
+        );
+        frontend.seed(
+            new Position(0, 1)
+        );
+
+        expect(frontend.get()).to.be.deep.equal([
+            new Position(1, 0),
+            new Position(1, 1),
+            new Position(1, 2)
+        ]);
+
+        console.log(frontend.toString());
+        frontend.elapse();
+
+        expect(frontend.get()).to.be.deep.equal([
+            new Position(1, 1),
+            new Position(0, 1),
+            new Position(2, 1)
+        ]);        
+        
+        console.log(frontend.toString());
+        frontend.elapse();
+
+        expect(frontend.get()).to.be.deep.equal([
+            new Position(1, 1),
+            new Position(1, 0),
+            new Position(1, 2)
+        ]);
+
+        console.log(frontend.toString());        
+    });
+
+});
