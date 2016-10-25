1ba919b33e283a0406b4c262b9442b62f3d6ce7a
Bastian Pertz <bastian.pertz@vodafone.com>
2016-10-25 23:31:11 +0200

    a function that get all neighbour positions

---
+++ lib/neighbours.ts
@@ -0,0 +1,40 @@
+import Position from './position';
+
+export default function getNeighbours(position: Position):Position[] {    
+    let neighbours:Position[] = [];
+
+    let x:number = position.x;
+    let y:number = position.y;
+
+    // neighbours on bottom
+    neighbours.push(
+        new Position( x - 1, y - 1)
+    );
+    neighbours.push(
+        new Position( x, y - 1)
+    );
+    neighbours.push(
+        new Position(  x + 1, y - 1)
+    );
+
+    // neighbours on the left and on the right
+    neighbours.push(
+        new Position( x - 1, y)
+    );
+    neighbours.push(
+        new Position( x + 1, y)
+    );
+
+    // neighbours on top
+    neighbours.push(
+        new Position( x - 1, y + 1)
+    );
+    neighbours.push(
+        new Position( x, y + 1)
+    );
+    neighbours.push(
+        new Position( x + 1, y + 1)
+    );
+
+    return neighbours;
+}
