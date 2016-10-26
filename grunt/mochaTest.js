4a15712d91f6b217d30c3f37ad49a964fe8508ae
Bastian Pertz <bastian.pertz@vodafone.com>
2016-10-26 22:45:45 +0200

    use source maps for correct trace stack

--- grunt/mochaTest.js
+++ grunt/mochaTest.js
@@ -2,6 +2,7 @@ var path = require('path');
 
 var mocha = {
     options: {
+        require: 'source-map-support/register',
         reporter: 'spec',
         quiet: false, // Optionally suppress output to standard out (defaults to false)
         clearRequireCache: true // Optionally clear the require cache before running tests (defaults to false)
