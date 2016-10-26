a234e2a00de618aa9d6bbcffb5dc713f52353538
Bastian Pertz <bastian.pertz@vodafone.com>
2016-10-26 23:42:39 +0200

    correct path to typescripts

--- grunt/lcov_sourcemap.js
+++ grunt/lcov_sourcemap.js
@@ -1,26 +1,39 @@
 module.exports = function(grunt){
 
+    var fs = require("fs");
     var lcovSourcemap = require("lcov-sourcemap");
 
     grunt.task.registerTask('lcov', 'lcov from source_map', function() {
 
         var done = this.async();
-        console.log('lcov')
 
-        lcovSourcemap.writeLcov(
+        lcovSourcemap(
             "./coverage/lcov.info", 
             [
-                './javascript/testing/lib/cell.js.map',
-                './javascript/testing/lib/frontend.js.map',
-                './javascript/testing/lib/habitat.js.map',
-                './javascript/testing/lib/livingcell.js.map',
-                './javascript/testing/lib/neighbours.js.map',
-                './javascript/testing/lib/position.js.map'                
+                'javascript/testing/lib/cell.js.map',
+                'javascript/testing/lib/frontend.js.map',
+                'javascript/testing/lib/habitat.js.map',
+                'javascript/testing/lib/livingcell.js.map',
+                'javascript/testing/lib/neighbours.js.map',
+                'javascript/testing/lib/position.js.map'                
             ],
             "./javascript/testing/lib/", 
-            "./coverage/lcov-mapped.info"
-        ).then(function () {
-            done();
+            "./"
+        ).then(function (lcov) {
+
+            lcov = lcov.replace(/.\/..\/..\/..\/lib/g, 'lib');
+
+            fs.writeFile('coverage/lcov-mapped.info', lcov, function(err) {
+                if (err) {
+                    throw err;
+                }
+
+                console.log('It\'s saved!');
+                done();
+            });
+            
+
+            //done();
         });
 
     });
