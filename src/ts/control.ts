dae21e819a81c416952f1d83b511fbc65630ec10
Bastian Pertz <bastian.pertz@vodafone.com>
2016-11-10 00:03:06 +0100

    dry

--- src/ts/control.ts
+++ src/ts/control.ts
@@ -66,28 +66,10 @@ export default class Control {
         const pan = $('<div>');
         pan.addClass('pan');
 
-        const top = $('<div>');
-        top.addClass('top');
-        top.append($('<i class="fa fa-arrow-up">'));
-        $(pan).append(top);
-
-        const bottom = $('<div>');
-        bottom.addClass('bottom');
-        bottom.append($('<i class="fa fa-arrow-down">'));
-        $(pan).append(bottom);
-
-        const left = $('<div>');
-        left.addClass('left');
-        left.append($('<i class="fa fa-arrow-left">'));
-        $(pan).append(left);
-
-        const right = $('<div>');
-        right.addClass('right');
-        right.append($('<i class="fa fa-arrow-right">'));
-        $(pan).append(right);
-
-        $(this.control).append(pan);
-
+        $(pan).append(this.getControlElement('pan', 'top', 'arrow-up'));
+        $(pan).append(this.getControlElement('pan', 'bottom', 'arrow-down'));
+        $(pan).append(this.getControlElement('pan', 'left', 'arrow-left'));
+        $(pan).append(this.getControlElement('pan', 'right', 'arrow-right'));
     }
 
     private createZoomControl():void{
@@ -95,17 +77,8 @@ export default class Control {
         const zoom = $('<div>');
         zoom.addClass('zoom');
 
-        const zoomIn = $('<div>');
-        zoomIn.addClass('zoom-in');
-        zoomIn.append($('<i class="fa fa-search-plus">'));
-        $(zoom).append(zoomIn);
-
-        const zoomOut = $('<div>');
-        zoomOut.addClass('zoom-out');
-        zoomOut.append($('<i class="fa fa-search-minus">'));
-        $(zoom).append(zoomOut);
-
-        $(this.control).append(zoom);
+        $(zoom).append(this.getControlElement('zoom', 'zoom-in', 'search-plus'));
+        $(zoom).append(this.getControlElement('zoom', 'zoom-out', 'search-minus'));
 
     }
 
