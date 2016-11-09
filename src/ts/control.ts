d1ebe71fe0ef420f4badc6516138ae81a96e1e8d
Bastian Pertz <bastian.pertz@vodafone.com>
2016-11-09 23:57:02 +0100

    dry repeated code into method getControlElement

--- src/ts/control.ts
+++ src/ts/control.ts
@@ -27,6 +27,16 @@ export default class Control {
 
     }
 
+    private getControlElement(action:string, value:string, icon:string):JQuery{
+        const element = $('<div>');
+        element.addClass(value);
+        element.attr('data-action', action);
+        element.attr('data-value', value);
+        element.append($('<i class="fa fa-' +  icon+ '">'));
+
+        return element
+    }
+
     private createPanControl():void{
 
         const pan = $('<div>');
