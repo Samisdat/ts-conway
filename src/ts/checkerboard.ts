2baaa384a7fdd3250e75b53697953a328f577260
Bastian Pertz <bastian.pertz@vodafone.com>
2016-11-12 01:01:11 +0100

    Vertical pannable checkerboard

--- src/ts/checkerboard.ts
+++ src/ts/checkerboard.ts
@@ -10,18 +10,18 @@ export default class Checkerboard {
     private cellWidth: number;
     private pan: Position;
 
-    /*
-    private startColors:string[][] = [
-        ['#FFAAAA', '#D46A6A'],
-        ['#D46A6A', '#FFAAAA']
-    ];*/
-
+    private startColors:string[][];
 
     constructor(canvas: Canvas, darkColor:string, lightColor:string) {
         this.canvas = canvas;
 
         this.darkColor = darkColor;
         this.lightColor = lightColor;
+
+        this.startColors = [
+            [this.darkColor, this.lightColor],
+            [this.lightColor, this.darkColor]
+        ];
     }
 
     public update(cellWidth:number, pan: Position){
@@ -29,37 +29,47 @@ export default class Checkerboard {
         this.pan = pan;
     }
 
+    private getStartColor():{ background: string; foreground: string; }{
+        let colors = {
+            background: '',
+            foreground: ''
+        }
+
+        const rowIndex = Math.abs(this.pan.y % 2);
+        const colIndex = Math.abs(this.pan.x % 2);
+
+        colors.background = this.startColors[rowIndex][colIndex];
+        colors.foreground = (this.darkColor === colors.background) ? this.lightColor : this.darkColor;
+
+        colors.background = this.darkColor;
+        colors.foreground = this.lightColor;
+
+        return colors;
+    }
+
     public render():void{
 
-        this.canvas.ctx.fillStyle = this.darkColor;   
+        const colors = this.getStartColor();
+    
+        this.canvas.ctx.fillStyle = colors.background;   
         this.canvas.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
 
-        const rows = Math.floor(this.canvas.height / this.cellWidth) + 2;
-        const cols = Math.floor(this.canvas.width / this.cellWidth) + 2;
+        const rows = Math.floor(this.canvas.height / this.cellWidth);
+        const cols = Math.floor(this.canvas.width / this.cellWidth);
 
-        console.log(cols, rows)
+        let xStart = this.pan.x - rows;
+        let xStop = xStart + cols + rows;
 
-        let xStart = - 2 - this.pan.x;
-        let xStop = xStart + cols + 4;
+        let yStart = this.pan.y;
+        let yStop = yStart + rows;
 
-        let yStart = - 2 - this.pan.y;
-        let yStop = yStart + rows + 4;
-
-
-        xStart = this.pan.x - rows;
-        xStop = cols + this.pan.x;
-
-        yStart = this.pan.y;
-        yStop = rows + this.pan.y;
-
-        this.canvas.ctx.fillStyle = this.lightColor;   
+        this.canvas.ctx.fillStyle = colors.foreground;   
 
         let positions:Position[] = [];
 
         for(let y = yStart; y < yStop + 1; y += 1){
 
             let tilePos = new Position(y, y);
-            tilePos = tilePos.move(this.pan);
 
             positions.push(tilePos);
             
