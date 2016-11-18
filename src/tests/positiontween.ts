82832bf527ca61c97681b309b8a5463da2de80f1
Bastian Pertz <bastian.pertz@vodafone.com>
2016-11-18 22:32:10 +0100

    a tween module for position

---
+++ src/tests/positiontween.ts
@@ -0,0 +1,123 @@
+import { expect } from 'chai';
+
+import Position from '../ts/position';
+import PositionTween from '../ts/positiontween';
+
+describe('PositionTween', () => {
+
+    beforeEach(function () {
+    });
+
+    it('can be created', () => {
+
+        let tween =  new PositionTween(
+            new Position(1, 1)    
+        );
+
+        expect(tween).to.be.instanceof(PositionTween);        
+        expect(tween.getStart()).to.be.deep.equal(new Position(1, 1));                
+        expect(tween.getCurrent()).to.be.deep.equal(new Position(1, 1));                
+        expect(tween.getEnd()).to.be.deep.equal(new Position(1, 1));
+
+    });
+
+    it('one step', () => {
+
+        let tween =  new PositionTween(
+            new Position(0, 0)    
+        );
+        tween.setEnd(
+            new Position(30, 60)            
+        );
+
+        expect(tween.getStart()).to.be.deep.equal(new Position(0, 0));                
+        expect(tween.getCurrent()).to.be.deep.equal(new Position(0, 0));                
+        expect(tween.getEnd()).to.be.deep.equal(new Position(30, 60));                
+
+        tween.update();
+
+        expect(tween.getCurrent()).to.be.deep.equal(new Position(1, 2));                
+        
+    });
+
+    it('from and to is the same', () => {
+
+        let tween =  new PositionTween(
+            new Position(0, 0)    
+        );
+        tween.setEnd(
+            new Position(0, 0)            
+        );
+
+        expect(tween.getStart()).to.be.deep.equal(new Position(0, 0));                
+        expect(tween.getCurrent()).to.be.deep.equal(new Position(0, 0));                
+        expect(tween.getEnd()).to.be.deep.equal(new Position(0, 0));                
+
+        tween.update();
+
+        expect(tween.getCurrent()).to.be.deep.equal(new Position(0, 0));                
+
+    });
+
+    it('all steps with integer values', () => {
+
+        var steps = 10;
+
+        let tween =  new PositionTween(
+            new Position(0, 0),
+            steps    
+        );
+        tween.setEnd(
+            new Position(10, 20)            
+        );
+
+        expect(tween.getStart()).to.be.deep.equal(new Position(0, 0));                
+        expect(tween.getCurrent()).to.be.deep.equal(new Position(0, 0));                
+        expect(tween.getEnd()).to.be.deep.equal(new Position(10, 20));                
+
+        for(let i = 0; i < steps; i += 1){
+            expect(tween.getCurrent()).to.be.deep.equal(new Position(i, i * 2));                
+            tween.update();
+        }
+        
+        expect(tween.getCurrent()).to.be.deep.equal(new Position(10, 20));                
+        tween.update();
+        expect(tween.getCurrent()).to.be.deep.equal(new Position(10, 20));                
+        tween.update();
+        expect(tween.getCurrent()).to.be.deep.equal(new Position(10, 20));                
+
+
+    });
+
+    it('all steps with float values', () => {
+
+        var steps = 100;
+
+        let tween =  new PositionTween(
+            new Position(0, 0),
+            steps    
+        );
+
+        tween.setEnd(
+            new Position(1, 2)            
+        );
+
+        expect(tween.getStart()).to.be.deep.equal(new Position(0, 0));                
+        expect(tween.getCurrent()).to.be.deep.equal(new Position(0, 0));                
+        expect(tween.getEnd()).to.be.deep.equal(new Position(1, 2));                
+
+        for(let i = 0; i <= steps; i += 1){
+            expect(Math.round(tween.getCurrent().x * 100)).to.be.equal(i);
+            expect(Math.round(tween.getCurrent().y * 100)).to.be.equal(i * 2);
+            tween.update();
+        }
+        
+        expect(tween.getCurrent()).to.be.deep.equal(new Position(1, 2));                
+        tween.update();
+        expect(tween.getCurrent()).to.be.deep.equal(new Position(1, 2));                
+        tween.update();
+        expect(tween.getCurrent()).to.be.deep.equal(new Position(1, 2));                
+
+    });
+
+});
---
+++ src/ts/positiontween.ts
@@ -0,0 +1,54 @@
+import Position from './position';
+import Tween from './tween';
+
+export default class PositionTween{
+
+    private steps: number;
+
+    private x: Tween;
+    private y: Tween;
+   
+    constructor(start: Position, steps = 30) {
+
+        this.steps = steps;
+
+        this.x = new Tween(start.x, this.steps);
+        this.y = new Tween(start.x, this.steps);
+    }
+
+    public equal(position: Position): Boolean {
+        return (position.x === this.x.getEnd() && position.y === this.y.getEnd())  ;
+    }
+    public setEnd(end: Position): void {
+
+        this.x.setEnd(end.x);
+        this.y.setEnd(end.y);
+    }
+
+    public getStart(): Position {
+        return new Position(
+            this.x.getStart(),
+            this.y.getStart(),
+        );
+    }
+    public getCurrent(): Position {
+        return new Position(
+            this.x.getCurrent(),
+            this.y.getCurrent(),
+        );
+    }
+    public getEnd(): Position {
+        return new Position(
+            this.x.getEnd(),
+            this.y.getEnd(),
+        );
+    }
+
+    public update():void {
+
+        this.x.update();
+        this.y.update();
+
+    }
+
+}
