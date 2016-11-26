8f64e28e661f8661155783da31564544c58c11d1
Bastian Pertz <bastian.pertz@vodafone.com>
2016-11-26 22:52:38 +0100

    a test with jQuery

--- typescript/tests/frontend.ts
+++ typescript/tests/frontend.ts
@@ -1,11 +1,24 @@
 import { expect } from 'chai';
 
+import jsdom = require('jsdom');
+var document = jsdom.jsdom('');
+var window = document.defaultView;
+global.window = window
+global.$ = require('jquery');
+
 import Frontend from '../src/frontend';
 
 describe('Frontend', () => {
 
     it('can be created', () => {
 
+        console.log($)
+
+        $.each([1,2,3], function(){
+
+            console.log(this)
+
+        })
 
     });
 
