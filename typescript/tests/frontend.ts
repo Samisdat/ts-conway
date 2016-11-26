7f5174a292d86a8bd223f6eed915b38ed53ccf3a
Bastian Pertz <bastian.pertz@vodafone.com>
2016-11-26 23:23:06 +0100

    test creation of frontend incl jquery

--- typescript/tests/frontend.ts
+++ typescript/tests/frontend.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 
 import jsdom = require('jsdom');
-var document = jsdom.jsdom('');
+var document = jsdom.jsdom('<html><body></body></html>');
 var window = document.defaultView;
 global.window = window
 global.$ = require('jquery');
@@ -10,16 +10,30 @@ import Frontend from '../src/frontend';
 
 describe('Frontend', () => {
 
-    it('can be created', () => {
+    afterEach( () => {
+        $('body').html('');
+    });
 
-        console.log($)
+    it('create', () => {
 
-        $.each([1,2,3], function(){
+        $('body').append(
+            $('<div id="conway">')
+        );        
 
-            console.log(this)
+        const frontend = new Frontend($('#conway'));
 
-        })
+        expect(frontend).to.be.instanceof(Frontend);
 
     });
+
+    it('creation fails with not dom elem', () => {
+
+        let createFrontend = function(){
+            const frontend = new Frontend($('#conway'));
+        };
+
+        expect(createFrontend).to.throw(Error, 'jquery selector does not match an element');
+    });
+
 
 });
