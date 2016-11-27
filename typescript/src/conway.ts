1d9e3337710a4f4bba7d41381f2715d2b1799b44
Bastian Pertz <bastian.pertz@vodafone.com>
2016-11-27 23:49:49 +0100

    conway only start frontend

--- typescript/src/conway.ts
+++ typescript/src/conway.ts
@@ -1,30 +1,12 @@
 import * as $ from 'jquery';
 
-import Grid from './grid';
-import CheckerBoard from './checkerboard';
+import Frontend from './frontend';
 
 
 import CanvasRenderer from './canvas-renderer';
 
 $(document).ready(function(){
 
-    let checkerboard = new CheckerBoard();
-
-    let canvasRenderer = new CanvasRenderer(
-        $('#conway')
-    );
-
-    console.log($('#conway').width()/30)
-    console.log()
-
-    let grid = new Grid(
-        $('#conway').height()/30, 
-        $('#conway').width()/30 
-    );
-    
-    checkerboard.update(grid);
-    canvasRenderer.update(30, grid);
-
-    canvasRenderer.render();
+    new Frontend($('#conway'));
 
 });
