7a0f91ee6b5951c80275b6db876efc4fa9c282ba
Bastian Pertz <bastian.pertz@vodafone.com>
2016-12-03 22:41:51 +0100

    grid-cel with witdh, height and position

--- typescript/src/grid-cell.ts
+++ typescript/src/grid-cell.ts
@@ -1,26 +1,38 @@
 import { GridCellType } from './grid-cell-types/grid-cell-type';
 import NoType from './grid-cell-types/no-type';
 
+import Position from './position';
+
 export default class GridCell {
 
-    private row: number;
-    private col: number;
+    private _x: number;
+    private _y: number;
+    private _width: number;
+    private _height: number;
 
     private type:GridCellType = new NoType();
 
-    constructor(col:number, row:number) {
+    constructor(position: Position, cellDimension: number) {
 
-        this.col = col;
-        this.row = row;
+        this._x = position.x;
+        this._y = position.y;
 
-    }
+        if(0 < this._x){
+            this._width = cellDimension;
+        }
+        else{
+            this._width = cellDimension + position.x;
+            this._x = 0;
+        }
 
-    public getRow():number{
-        return this.row;
-    }
+        if(0 < this._y){
+            this._height = cellDimension;
+        }
+        else{
+            this._height = cellDimension + position.y;
+            this._y = 0;
+        }
 
-    public getCol():number{
-        return this.col;
     }
 
     public setType(type:GridCellType):void{
@@ -35,4 +47,16 @@ export default class GridCell {
         return this.type.toHex();
     }
 
+    get width():number{
+        return this._width;
+    }
+    get height():number{
+        return this._height;
+    }
+    get x():number{
+        return this._x;
+    }
+    get y():number{
+        return this._y;
+    }
 }
--- typescript/tests/grid-cell.ts
+++ typescript/tests/grid-cell.ts
@@ -1,35 +1,73 @@
 import { expect } from 'chai';
 
 import Position from '../src/position';
+import LivingCell from '../src/grid-cell-types/living-cell';
+
 import GridCell from '../src/grid-cell';
+
 
 describe('GridCell', () => {
 
     it('can be created', () => {
 
-        let gridCell = new GridCell(3, 3);
+        let gridCell = new GridCell(new Position(0, 0), 100);
 
         expect(gridCell).to.be.instanceof(GridCell);
 
     });
 
-    it('get row and col', () => {
+    it('getter x, y, width and height for positive position ', () => {
 
-        let gridCell = new GridCell(3, 4);
+        let gridCell = new GridCell(new Position(10, 10), 100);
 
-        expect(gridCell.getRow()).to.be.equal(4);
-        expect(gridCell.getCol()).to.be.equal(3);
+        expect(gridCell.x).to.be.equal(10);
+        expect(gridCell.y).to.be.equal(10);
+        expect(gridCell.width).to.be.equal(100);
+        expect(gridCell.height).to.be.equal(100);
+
+    });
+
+    it('getter x, y, width and height for positive position.x and negative y', () => {
+
+        let gridCell = new GridCell(new Position(0, -50), 100);
+
+        expect(gridCell.x).to.be.equal(0);
+        expect(gridCell.y).to.be.equal(0);
+        expect(gridCell.width).to.be.equal(100);
+        expect(gridCell.height).to.be.equal(50);
+
+    });
+
+    it('getter x, y, width and height for negative position.x and positive y', () => {
+
+        let gridCell = new GridCell(new Position(-50, 0), 100);
+
+        expect(gridCell.x).to.be.equal(0);
+        expect(gridCell.y).to.be.equal(0);
+        expect(gridCell.width).to.be.equal(50);
+        expect(gridCell.height).to.be.equal(100);
+
+    });
+
+    it('getter x, y, width and height for negative position.x and negative y', () => {
+
+        let gridCell = new GridCell(new Position(-50, -25), 100);
+
+        expect(gridCell.x).to.be.equal(0);
+        expect(gridCell.y).to.be.equal(0);
+        expect(gridCell.width).to.be.equal(50);
+        expect(gridCell.height).to.be.equal(75);
 
     });
 
     it('get/set color', () => {
 
-        let gridCell = new GridCell(3, 3);
+        let gridCell = new GridCell(new Position(0, 0), 100);
 
         expect(gridCell.getColor()).to.be.undefined;
 
-        gridCell.setColor('#f00');
-        expect(gridCell.getColor()).to.be.equal('#f00');
+        gridCell.setType(new LivingCell());
+        expect(gridCell.getColor()).to.be.equal('#000000');
 
     });
 
