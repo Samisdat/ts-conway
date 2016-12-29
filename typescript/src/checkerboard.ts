74052f563d5b009d8af74625149f2d926941671b
Bastian Pertz <bastian.pertz@vodafone.com>
2016-12-29 23:28:43 +0100

    coloring checkerboard

--- typescript/src/checkerboard.ts
+++ typescript/src/checkerboard.ts
@@ -23,57 +23,28 @@ export default class Checkerboard{
 
     public update(grid:Grid): void {
 
-        const end = new Position(
-            Math.ceil((grid.getCols() -1) / 2),
-            Math.ceil((grid.getRows() - 1)  / 2)
-        );
+        const cells = grid.getCells();
 
-        let start = new Position(
-            -1 * end.x,
-            -1 * end.y
-        );
+        console.log(cells);
 
-        let firstColorOfRow:GridCellType = this.lightColor;
+        for(let cell of cells){
+            const x = cell.x;
+            const y = cell.y;
 
-        let color:GridCellType = firstColorOfRow;
-        
-        while(start.y < grid.getRows()/ 2){
+            cell.setType(this.darkColor);
 
-            while(start.x < grid.getCols()/ 2){
-
-                grid.getCell(start.x, start.y).setType(color);
-
-                color = (this.lightColor === color) ? this.darkColor: this.lightColor;
-                
-                start = start.move(new Position(1, 0));
-                
+            if(0 === y % 2){
+                if(0 === x % 2){
+                    cell.setType(this.lightColor);
+                }
             }
 
-            firstColorOfRow = (this.lightColor === firstColorOfRow) ? this.darkColor: this.lightColor;
-            color = firstColorOfRow;
-
-            start = new Position(-1 * end.x, start.y + 1);
-            
-        }
-        
-        /*
-        let firstColorOfRow:GridCellType = this.lightColor;
-
-        let color:GridCellType = firstColorOfRow;
-    
-        for(let row = 0; row < grid.getRows(); row += 1){
-            
-            for(let col = 0; col < grid.getCols(); col += 1){
-                
-                grid.getCell(col, row).setType(color);
-
-                color = (this.lightColor === color) ? this.darkColor: this.lightColor;
-                
+            if(0 !== y % 2){
+                if(0 !== x % 2){
+                    cell.setType(this.lightColor);
+                }
             }
-            firstColorOfRow = (this.lightColor === firstColorOfRow) ? this.darkColor: this.lightColor;
-            color = firstColorOfRow;
         }
-        */
 
     }
 
