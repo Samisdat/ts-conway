38a555ac9333238b2855886363f9f4cf414d5e59
Bastian Pertz <bastian.pertz1@vodafone.com>
2017-10-23 21:12:42 +0000

    Use node typescript binding to run tests

--- typescript/tests/_first.ts
+++ typescript/tests/_first.ts
@@ -2,6 +2,8 @@
  * While this test is named _first.ts it while run as first test and is used to setup jQuery
  */
 
+/// <reference path="../src/global.d.ts" />
+
 import jsdom = require('jsdom');
 let document = jsdom.jsdom('<html><body></body></html>');
 let window = document.defaultView;
