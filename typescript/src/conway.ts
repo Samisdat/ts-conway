c48ef7b3c82a8aced08877fac958195d5bed2933
samisdat <bastian@pertz.eu>
2017-12-20 13:14:24 +0000

    Use only one game

--- typescript/src/conway.ts
+++ typescript/src/conway.ts
@@ -9,10 +9,12 @@ $(document).ready(function(){
 
     new Frontend($('#conway1'));
 
+    /*
     new Frontend($('#conway2'));
 
     new Frontend($('#conway3'));
 
     new Frontend($('#conway4'));
+    */
 
 });
