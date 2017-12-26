9e55415c63f7c1f921ae1b3a53bbac0e2cb39421
samisdat <bastian@pertz.eu>
2017-12-26 22:50:30 +0000

    GridCell know it's absolute position and render checkerboard pattern

--- typescript/grid-cell.test.ts
+++ typescript/grid-cell.test.ts
@@ -5,12 +5,17 @@ import LivingCell from './grid-cell-types/living-cell';
 
 import GridCell from './grid-cell';
 
+import CheckerboardDark from './grid-cell-types/checkerboard-dark';
+import CheckerboardLight from './grid-cell-types/checkerboard-light';
+
+const darkColor: CheckerboardDark = new CheckerboardDark();
+const lightColor: CheckerboardLight = new CheckerboardLight();
 
 describe('GridCell', () => {
 
     it('can be created', () => {
 
-        let gridCell = new GridCell(new Position(0, 0));
+        let gridCell = new GridCell(new Position(0, 0), new Position(0, 0));
 
         expect(gridCell).to.be.instanceof(GridCell);
 
@@ -18,21 +23,52 @@ describe('GridCell', () => {
 
     it('getter x, y, width and height for positive position ', () => {
 
-        let gridCell = new GridCell(new Position(10, 10));
+        let gridCell = new GridCell(new Position(10, 10), new Position(0, 0));
 
         expect(gridCell.x).to.be.equal(10);
         expect(gridCell.y).to.be.equal(10);
 
     });
 
-    it('get/set color', () => {
+    it('get checkerboard color', () => {
 
-        let gridCell = new GridCell(new Position(0, 0));
+        let gridCell: GridCell;
 
-        expect(gridCell.getColor()).to.be.undefined;
+        gridCell = new GridCell(new Position(-1, -1), new Position(0, 0));
+        expect(gridCell.getColor()).to.be.equal(lightColor.toHex());
 
-        gridCell.setType(new LivingCell());
-        expect(gridCell.getColor()).to.be.equal('#000000');
+        gridCell = new GridCell(new Position(0, -1), new Position(0, 0));
+        expect(gridCell.getColor()).to.be.equal(darkColor.toHex());
+
+        gridCell = new GridCell(new Position(1, -1), new Position(0, 0));
+        expect(gridCell.getColor()).to.be.equal(lightColor.toHex());
+        
+        gridCell = new GridCell(new Position(-1, 0), new Position(0, 0));
+        expect(gridCell.getColor()).to.be.equal(darkColor.toHex());
+
+        gridCell = new GridCell(new Position(0, 0), new Position(0, 0));
+        expect(gridCell.getColor()).to.be.equal(lightColor.toHex());
+
+        gridCell = new GridCell(new Position(1, 0), new Position(0, 0));
+        expect(gridCell.getColor()).to.be.equal(darkColor.toHex());
+
+        gridCell = new GridCell(new Position(-1, 1), new Position(0, 0));
+        expect(gridCell.getColor()).to.be.equal(lightColor.toHex());
+
+        gridCell = new GridCell(new Position(0, 1), new Position(0, 0));
+        expect(gridCell.getColor()).to.be.equal(darkColor.toHex());
+
+        gridCell = new GridCell(new Position(1, 1), new Position(0, 0));
+        expect(gridCell.getColor()).to.be.equal(lightColor.toHex());
+        
+        
+    });
+
+    it('getGridOffset', () => {
+
+        let gridCell = new GridCell(new Position(0, 0), new Position(1, 1));
+
+        expect(gridCell.getGridOffset()).to.be.deep.equal(new Position(1, 1));
 
     });
 
--- typescript/grid-cell.ts
+++ typescript/grid-cell.ts
@@ -1,6 +1,13 @@
 import { GridCellType } from './grid-cell-types/grid-cell-type';
 import NoType from './grid-cell-types/no-type';
 
+import CheckerboardDark from './grid-cell-types/checkerboard-dark';
+import CheckerboardLight from './grid-cell-types/checkerboard-light';
+
+const darkColor: CheckerboardDark = new CheckerboardDark();
+const lightColor: CheckerboardLight = new CheckerboardLight();
+
+
 import Position from './position';
 
 export default class GridCell {
@@ -10,11 +17,40 @@ export default class GridCell {
 
     private type: GridCellType = new NoType();
 
-    constructor(position: Position, gridOffset: Position ) {
+    constructor(position: Position, gridOffset: Position) {
 
         this.position = position;
         this.gridOffset = gridOffset;
 
+        this.setCheckerboardColor();
+
+    }
+
+    private setCheckerboardColor():void{
+
+        const absolutePosition = new Position(
+            this.position.x - this.gridOffset.x,
+            this.position.y - this.gridOffset.y
+        );
+
+        const x = absolutePosition.x;
+        const y = absolutePosition.y;
+
+        this.setType(darkColor);
+
+        if (0 === y % 2) {
+            if (0 === x % 2) {
+                this.setType(lightColor);
+            }
+        }
+
+        if (0 !== y % 2) {
+            if (0 !== x % 2) {
+                this.setType(lightColor);
+            }
+        }
+
+        
     }
 
     public setType(type: GridCellType): void {
@@ -34,5 +70,9 @@ export default class GridCell {
     }
     get y(): number {
         return this.position.y;
+    }
+
+    public getGridOffset(): Position {
+        return this.gridOffset
     }
 }
