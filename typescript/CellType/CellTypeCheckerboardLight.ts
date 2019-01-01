f99c16b78e0e192ccd1526906e7744411ead5829
samisdat <bastian@pertz.eu>
2019-01-01 23:15:41 +0000

    Tslint

--- typescript/CellType/CellTypeCheckerboardLight.ts
+++ typescript/CellType/CellTypeCheckerboardLight.ts
@@ -2,7 +2,7 @@ import { CellTypeInterface } from 'CellType/CellTypeInterface';
 
 export class CellTypeCheckerboardLight implements CellTypeInterface {
 
-    public readonly name:string = 'checkerboard-light';
+    public readonly name: string = 'checkerboard-light';
 
     public readonly hex: string = '#FFAAAA';
 
