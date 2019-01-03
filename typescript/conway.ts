3521b99b2876e058e171a92e06dafc95ea987270
samisdat <bastian@pertz.eu>
2019-01-03 06:14:43 +0000

    two canvas with one additional synced controll

--- typescript/conway.ts
+++ typescript/conway.ts
@@ -6,12 +6,30 @@ $(document).ready(function() {
 
     new Frontend($('#conway1'));
 
-    /*
     new Frontend($('#conway2'));
 
-    new Frontend($('#conway3'));
+    $('#sync').find('.pan .top').click(() => {
+        $('#conway1 .pan .top, #conway2 .pan .top').click();
+    });
 
-    new Frontend($('#conway4'));
-    */
+    $('#sync').find('.pan .bottom').click(() => {
+        $('#conway1 .pan .bottom, #conway2 .pan .bottom').click();
+    });
+
+    $('#sync').find('.pan .left').click(() => {
+        $('#conway1 .pan .left, #conway2 .pan .left').click();
+    });
+
+    $('#sync').find('.pan .right').click(() => {
+        $('#conway1 .pan .right, #conway2 .pan .right').click();
+    });
+
+    $('#sync').find('.zoom-in').click(() => {
+        $('#conway1 .zoom-in, #conway2 .zoom-in').click();
+    });
+
+    $('#sync').find('.zoom-out').click(() => {
+        $('#conway1 .zoom-out, #conway2 .zoom-out').click();
+    });
 
 });
