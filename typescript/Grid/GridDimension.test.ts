f2328147d159f761343571210aa7bce4a29f03b8
samisdat <bastian@pertz.eu>
2019-01-08 14:32:31 +0000

    tdd grid dimension

--- typescript/Grid/GridDimension.test.ts
+++ typescript/Grid/GridDimension.test.ts
@@ -14,6 +14,73 @@ describe('GridDimension', () => {
     });
 
 
+    it('rows can be retrieved', () => {
+
+        let gridDimension = new GridDimension(2, 3);
+
+        expect(gridDimension.rows).to.be.equal(2);
+
+    });
+
+    it('cols can be retrieved', () => {
+
+        let gridDimension = new GridDimension(2, 3);
+
+        expect(gridDimension.cols).to.be.equal(3);
+
+    });
+
+    it('throw execption for rows with decimal value', () => {
+
+        expect(() => {
+            new GridDimension(0.5, 3);
+        }).to.throw(
+            Error, 'rows must be an integer'
+        );
+
+    });
+
+    it('throw execption for cols with decimal value', () => {
+
+        expect(() => {
+            new GridDimension(2, 0.5);
+        }).to.throw(
+            Error, 'cols must be an integer'
+        );
+
+    });
+
+    it('throw execption for rows value less then 1', () => {
+
+        expect(() => {
+            new GridDimension(0, 3);
+        }).to.throw(
+            Error, 'rows must be at least 1'
+        );
+
+        expect(() => {
+            new GridDimension(-1, 3);
+        }).to.throw(
+            Error, 'rows must be at least 1'
+        );
+
+    });
+
+    it('throw execption for cols value less then 1', () => {
+
+        expect(() => {
+            new GridDimension(3, 0);
+        }).to.throw(
+            Error, 'cols must be at least 1'
+        );
+
+        expect(() => {
+            new GridDimension(3, -1);
+        }).to.throw(
+            Error, 'cols must be at least 1'
+        );
+
+    });
 
 
 });
--- typescript/Grid/GridDimension.ts
+++ typescript/Grid/GridDimension.ts
@@ -6,12 +6,31 @@ export class GridDimension {
 
     constructor(rows: number, cols: number) {
 
+        this.ensureInteger(rows, 'rows');
+        this.ensureInteger(cols, 'cols');
+
+        this.ensurePositiveNumber(rows, 'rows');
+        this.ensurePositiveNumber(cols, 'cols');
+
         this.rows = rows;
         this.cols = cols;
 
     }
 
-    private ensurePositiveNumber(value: number, message:string){
+    private ensureInteger(value: number, attributeName: string) {
+
+        if (0 !== value % 1) {
+            throw new Error(attributeName + ' must be an integer');
+        }
+
+    }
+
+    private ensurePositiveNumber(value: number, attributeName: string) {
+
+        if (1 > value) {
+            throw new Error(attributeName + ' must be at least 1');
+        }
+
 
     }
 }
