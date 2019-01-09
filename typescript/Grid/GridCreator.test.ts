2c1e68cee8b947835102bcb89fdcd7c398a34d43
samisdat <bastian@pertz.eu>
2019-01-09 20:39:57 +0000

    tdd GridCreator

--- typescript/Grid/GridCreator.test.ts
+++ typescript/Grid/GridCreator.test.ts
@@ -12,25 +12,128 @@ describe('GridCreator', () => {
             200,
             200,
             100,
-            new Position(0,0)
+            new Position(0,0),
+            1
         );
 
         expect(gridCreator).to.be.instanceof(GridCreator);
 
     });
 
-    it('create an easy case', () => {
+    it('absolute position and offset can be retrieved for integer pan', () => {
 
         let gridCreator = new GridCreator(
             100,
             100,
             100,
-            new Position(0,0)
+            new Position(1,3),
+            1
+        );
+
+        expect(gridCreator.getPan()).to.be.deep.equal(new Position(1, 3));
+        expect(gridCreator.getSourcePosition()).to.be.deep.equal(new Position(1, 3));
+        expect(gridCreator.getOffset()).to.be.deep.equal(new Position(0, 0));
+
+    });
+
+    it('absolute position and offset can be retrieved for 0.5/0.5 pan', () => {
+
+        let gridCreator = new GridCreator(
+            100,
+            100,
+            100,
+            new Position(0.5,0.5),
+            1
+        );
+
+        expect(gridCreator.getPan()).to.be.deep.equal(new Position(0.5, 0.5));
+        expect(gridCreator.getSourcePosition()).to.be.deep.equal(new Position(0, 0));
+        expect(gridCreator.getOffset()).to.be.deep.equal(new Position(0.5, 0.5));
+
+    });
+
+    it('absolute position and offset can be retrieved for 4.8/-3.3 pan', () => {
+
+        let gridCreator = new GridCreator(
+            100,
+            100,
+            100,
+            new Position(4.8,-3.3),
+            1
+        );
+
+        expect(gridCreator.getPan()).to.be.deep.equal(new Position(4.8, -3.3));
+        expect(gridCreator.getSourcePosition()).to.be.deep.equal(new Position(4, -3));
+        expect(gridCreator.getOffset().x).to.be.approximately(0.8, 0.0001);
+        expect(gridCreator.getOffset().y).to.be.approximately(-0.3, 0.0001);
+
+    });
+
+    it('an easy case', () => {
+
+        let gridCreator = new GridCreator(
+            100,
+            100,
+            100,
+            new Position(0,0),
+            1
         );
 
         expect(gridCreator.getRows()).to.be.equal(1);
         expect(gridCreator.getCols()).to.be.equal(1);
         expect(gridCreator.getPan()).to.be.deep.equal(new Position(0, 0));
+        expect(gridCreator.getZoom()).to.be.equal(1);
+
+    });
+
+    it('zoom a little', () => {
+
+        let gridCreator = new GridCreator(
+            150,
+            150,
+            100,
+            new Position(0,0),
+            0.5
+        );
+
+        expect(gridCreator.getRows()).to.be.equal(3);
+        expect(gridCreator.getCols()).to.be.equal(3);
+        expect(gridCreator.getPan()).to.be.deep.equal(new Position(0, 0));
+        expect(gridCreator.getZoom()).to.be.equal(0.5);
+
+    });
+
+    it('move a little to the left', () => {
+
+        let gridCreator = new GridCreator(
+            100,
+            100,
+            100,
+            new Position(-0.5,0),
+            1
+        );
+
+        expect(gridCreator.getRows()).to.be.equal(2);
+        expect(gridCreator.getCols()).to.be.equal(1);
+        expect(gridCreator.getPan()).to.be.deep.equal(new Position(-0.5, 0));
+        expect(gridCreator.getZoom()).to.be.equal(1);
+
+    });
+
+    it('move a little to the right', () => {
+
+        let gridCreator = new GridCreator(
+            100,
+            100,
+            100,
+            new Position(0.5,0),
+            1
+        );
+
+        expect(gridCreator.getRows()).to.be.equal(2);
+        expect(gridCreator.getCols()).to.be.equal(1);
+        expect(gridCreator.getPan()).to.be.deep.equal(new Position(0.5, 0));
+        expect(gridCreator.getZoom()).to.be.equal(1);
 
     });
 
--- typescript/Grid/GridCreator.ts
+++ typescript/Grid/GridCreator.ts
@@ -5,8 +5,14 @@ export class GridCreator {
 
     public readonly canvasWidth:number;
     public readonly canvasHeigth:number;
+    public readonly originalCellWidthAndHeight:number;
     public readonly cellWidthAndHeight:number;
     public readonly pan:Position;
+
+    public readonly sourcePosition:Position;
+    public readonly offset:Position;
+
+    public readonly zoom: number;
 
     private rows:number;
     private cols:number;
@@ -15,16 +21,36 @@ export class GridCreator {
         canvasWidth:number,
         canvasHeigth:number,
         cellWidthAndHeight:number,
-        pan:Position
+        pan:Position,
+        zoom:number
     ) {
         this.canvasWidth = canvasWidth;
         this.canvasHeigth = canvasHeigth;
-        this.cellWidthAndHeight = cellWidthAndHeight;
+        this.originalCellWidthAndHeight = cellWidthAndHeight;
         this.pan = pan;
+        this.zoom = zoom;
 
+        this.offset = new Position(
+            this.pan.x % 1,
+            this.pan.y % 1,
 
-        this.rows = this.canvasWidth / cellWidthAndHeight;
-        this.cols = this.canvasHeigth / cellWidthAndHeight;
+        );
+
+        this.sourcePosition = this.pan.clone().move(this.offset.inverse());
+
+        this.cellWidthAndHeight = this.originalCellWidthAndHeight * this.getZoom();
+        
+        this.rows = Math.ceil(this.canvasWidth / this.cellWidthAndHeight);
+
+        if(0 !== this.offset.x){
+            this.rows += 1;
+        }
+
+        this.cols = Math.ceil(this.canvasHeigth / this.cellWidthAndHeight);
+
+        if(0 !== this.offset.y){
+            this.cols += 1;
+        }
 
     }
 
@@ -38,6 +64,18 @@ export class GridCreator {
 
     public getPan():Position{
         return this.pan;
+    }
+
+    public getSourcePosition():Position{
+        return this.sourcePosition;
+    }
+
+    public getOffset():Position{
+        return this.offset;
+    }
+
+    public getZoom():number{
+        return this.zoom;
     }
 
 }
