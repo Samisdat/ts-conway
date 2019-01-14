a38c8b9cc991e93a3507e94da4a589bfbc5f7ddc
samisdat <bastian@pertz.eu>
2019-01-14 22:32:08 +0000

    description echo

--- copy-types.sh
+++ copy-types.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+echo "copy @types from container into host to make enable autocomplete etc in ide"
+
 docker cp conway_conway_1:/home/conway/node_modules/@types $(pwd)/node_modules
