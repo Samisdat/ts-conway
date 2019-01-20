b95581a95e6aa3ac4c8710a59d4f18fd134c4312
samisdat <bastian@pertz.eu>
2019-01-20 23:45:30 +0000

    seeding patterns

--- typescript/src/Frontend.ts
+++ typescript/src/Frontend.ts
@@ -43,9 +43,106 @@ export class Frontend {
             Config.cellWidth
         );
 
+        this.initialSeed();
+
         this.loop();
 
+
+
         this.habitat.startAging();
+
+        function checkWindowSize() {
+            var width = window.innerWidth,
+                height = window.innerHeight;
+
+            console.log("window.innerHeight: ", window.innerHeight, " window.innerWidth: ", window.innerWidth);
+        }
+
+        window.onload = () => {
+            checkWindowSize();
+            window.addEventListener('resize', (event) => {
+                checkWindowSize();
+
+                this.canvasRenderer.setCanvas()
+            });
+        }
+
+    }
+
+    private initialSeed():void{
+
+        const patterns = new Patterns();
+        const gunsAndEaters = patterns.get('guns_and_eaters');
+
+        const gridCreator = new GridCreator(
+            this.wrapper.width(),
+            this.wrapper.height(),
+            Config.cellWidth,
+            this.control.getPan(),
+            this.control.getZoom()
+        );
+
+        const patternGutter = 2;
+
+        let repeat = Math.floor(gridCreator.getRows() / (gunsAndEaters.getWidth() + patternGutter ));
+
+        if(0 === repeat % 2){
+            repeat += 1
+        }
+
+        let patternsPerSide = Math.floor(repeat/ 2);
+
+        this.habitat.seedPattern(
+            patterns.get('guns_and_eaters'),
+            new Position(0, 0)
+        );
+
+
+        let move = new Position(0, 0);
+
+        if(0 === repeat % 2) {
+            move = move.move(
+                new Position(-1* Math.floor((gunsAndEaters.getWidth() + patternGutter )  / 2 ), 0)
+            )
+        }
+
+        for(let i = 0; i < patternsPerSide; i += 1){
+
+            move = move.move(
+                new Position(
+                    -1 * (gunsAndEaters.getWidth() + patternGutter), 0
+                )
+            )
+
+            this.habitat.seedPattern(
+                patterns.get('guns_and_eaters'),
+                move
+            );
+
+        }
+
+        move = new Position(0, 0);
+
+        if(0 === repeat % 2) {
+            move = move.move(
+                new Position(-1* Math.floor((gunsAndEaters.getWidth() + patternGutter )  / 2 ), 0)
+            )
+        }
+
+        for(let i = 0; i < patternsPerSide; i += 1){
+
+            move = move.move(
+                new Position(
+                    gunsAndEaters.getWidth() + patternGutter, 0
+                )
+            );
+
+            this.habitat.seedPattern(
+                patterns.get('guns_and_eaters'),
+                move
+            );
+
+        }
 
     }
 
