66e6238963cc80eaed4e46a9efe67ce0752f71ee
Bastian Pertz <bastian.pertz1@vodafone.com>
2020-04-23 22:36:25 +0200

    Webpack config until local web server is running with html, css and js

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import './style.scss';
+
+import * as $ from 'jquery';
+
+import { Frontend } from '../typescript/src/Frontend';
+
+$(document).ready(function() {
+
+    new Frontend();
+
+});
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin  = require('html-webpack-plugin');
+
+var DashboardPlugin = require("webpack-dashboard/plugin");
+
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
+    plugins: [
+        new DashboardPlugin(),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin(
+            {
+                title: 'Conways game of live',
+                template: 'src/index.html'
+            }
+        )
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: [ '.ts', '.js' ],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
