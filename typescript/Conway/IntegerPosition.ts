6f7673c41a64a1eb111ccc631897a397800ec3e1
Bastian Pertz <bastian.pertz1@vodafone.com>
2020-05-04 23:01:38 +0200

    Ensure that IntegerPosition has x&y as integer

--- typescript/Conway/IntegerPosition.ts
+++ typescript/Conway/IntegerPosition.ts
@@ -9,6 +9,16 @@ export class IntegerPosition extends Position {
 
     }
 
+    protected ensureInteger() {
 
+        if (0 !== this.x % 1) {
+            throw new Error('x not an integer value');
+        }
+
+        if (0 !== this.y % 1) {
+            throw new Error('y not an integer value');
+        }
+
+    }
 
 }
