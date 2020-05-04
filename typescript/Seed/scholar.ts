7a001e335748ef93cc87bae0b087344d261abe87
Bastian Pertz <bastian.pertz1@vodafone.com>
2020-05-04 23:03:22 +0200

    Seed a scholar ship

---
+++ typescript/Seed/scholar.ts
@@ -0,0 +1,18 @@
+import {SeedFunction} from '@Conway/Frontend/Web';
+import {GridCreator} from '@Conway/Grid/GridCreator';
+import {CellMatrix} from '@Conway/Conway/CellMatrix';
+import {Patterns} from '@Conway/Conway/Patterns';
+import {Position} from '@Conway/Conway/Position';
+
+export const scholar: SeedFunction = (gridCreator: GridCreator, matrix: CellMatrix) => {
+
+    const patterns = new Patterns();
+    const pattern = patterns.get('scholar');
+
+    matrix.seedPattern(
+        pattern,
+        new Position(0, 0)
+    );
+
+};
+
