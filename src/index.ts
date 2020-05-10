3f1beb37d5f7f7bbe2c4d21b37349de7e6b61057
Bastian Pertz <bastian.pertz1@vodafone.com>
2020-05-10 21:15:52 +0200

    Seed scholar

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import {Web} from '@Conway/Frontend/Web';
 import {MainControl} from '@Conway/Control/ControlMain';
 import {CELL_WIDTH} from '@Conway/Constants';
 import {guns_and_eaters} from '@Conway/Seed/guns_and_eaters';
+import {scholar} from '@Conway/Seed/scholar';
 import {NoControl} from '@Conway/Control/ControlNo';
 
 
@@ -11,20 +12,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const element = document.getElementById('conway') as HTMLElement;
 
+    /*
     new Web(
         element,
         guns_and_eaters,
         new NoControl()
     );
+    */
 
-    /*
     new Web(
         element,
-        guns_and_eaters,
+        scholar,
         new MainControl(
             element,
             CELL_WIDTH
         )
     );
-    */
+
 });
