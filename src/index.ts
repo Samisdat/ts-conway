7be41d57cfc03b9b20f679e169a8cc958f13088f
Bastian Pertz <bastian.pertz1@vodafone.com>
2020-05-11 22:58:06 +0200

    Eslint

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import './style.scss';
 import {Web} from '@Conway/Frontend/Web';
 import {MainControl} from '@Conway/Control/ControlMain';
 import {CELL_WIDTH} from '@Conway/Constants';
-import {guns_and_eaters} from '@Conway/Seed/guns_and_eaters';
+import {gunsAndEaters} from '@Conway/Seed/guns_and_eaters';
 import {scholar} from '@Conway/Seed/scholar';
 import {NoControl} from '@Conway/Control/ControlNo';
 
--- typescript/Constants.ts
+++ typescript/Constants.ts
@@ -1,17 +1,17 @@
-export const CELL_WIDTH: number = 10;
+export const CELL_WIDTH = 10;
 
-export const GENERATION_DURATION: number = 500;
+export const GENERATION_DURATION = 200;
 
-export const DEBUG: boolean = false;
+export const DEBUG = false;
 
-export const COLOR_CHECKERBOARD_DARK: string = '#DFD2AE';
+export const COLOR_CHECKERBOARD_DARK = '#DFD2AE';
 
-export const COLOR_CHECKERBOARD_LIGHT: string = '#EBE3CD';
+export const COLOR_CHECKERBOARD_LIGHT = '#EBE3CD';
 
-export const COLOR_LIVING: string = '#DB8555';
+export const COLOR_LIVING = '#DB8555';
 
-export const COLOR_CENTER: string = '#ff0000';
+export const COLOR_CENTER = '#ff0000';
 
-export const PAN_TWEEN_STEPS: number = 10;
+export const PAN_TWEEN_STEPS = 10;
 
-export const ZOOM_TWEEN_STEPS: number = 10;
+export const ZOOM_TWEEN_STEPS = 10;
