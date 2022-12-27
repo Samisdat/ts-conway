0cbf4ca35b69b7f73b6996603597dc17bd4b616d
samisdat <bastian@pertz.eu>
2022-12-27 23:46:28 +0100

    add vitest as in this exanple https://github.com/vitest-dev/vitest/blob/main/examples/react/

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    include: ["src/**/*.test.{ts,tsx}"],
+  },
   plugins: [react()],
-})
+});
