a2db6c64f3208a3fe7e9136335c0b29cf29b9db9
samisdat <bastian@pertz.eu>
2022-12-28 00:24:37 +0100

    add styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+import { GlobalStyles } from "./styles/GlobalStyles";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
@@ -27,8 +29,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <GlobalStyles />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,7 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  body {
+      border:10px solid red;
+  }
+`;
